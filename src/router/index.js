866b0e1ff3cd975287fd6ecaef8f2aeabdf7bf33
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-07-14 00:01:49 +0900

    ログイン画面の作成

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 // import HomeView from "../views/HomeView.vue";
 import UserList from "../views/UserList.vue";
 import ChatBoard from "../views/ChatBoard.vue";
+import LoginView from "../views/LoginView.vue";
 
 Vue.use(VueRouter);
 
@@ -25,6 +26,11 @@ const routes = [
     path: "/chat",
     name: "ChatBoard",
     component: ChatBoard,
+  },
+  {
+    path: "/login",
+    name: "LoginView",
+    component: LoginView,
   },
 ];
 
